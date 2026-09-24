61a7797aa3a01066f72265fa9bdea90ae591ec4c
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-09-24 14:54:26 -0300

    Remove pipeline-pagos, and change app ports

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -3,6 +3,6 @@ import { BffModule } from './bff.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BffModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 5000);
 }
 bootstrap();
--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -3,6 +3,6 @@ import { CoreApiModule } from './core-api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreApiModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 4000);
 }
 bootstrap();
